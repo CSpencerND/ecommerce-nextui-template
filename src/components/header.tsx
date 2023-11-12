5f75749f58488e1dfc1ea121be065259199ef388
cspencer <cspencernd@gmail.com>
2023-11-12 06:16:12 -0500

    feat/style: add header, and adjust theme

---
+++ src/components/header.tsx
@@ -0,0 +1,104 @@
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
+} from "@nextui-org/navbar";
+
+import { siteConfig } from "@/config";
+
+const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+];
+
+type HeaderProps = {};
+
+export const Header: React.FC<HeaderProps> = () => (
+    <Navbar isBordered>
+        <NavbarContent>
+            <NavbarBrand>
+                <p className="text-2xl font-bold">{siteConfig.logo}</p>
+            </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent
+            className="flex gap-4 max-lg:hidden"
+            justify="center"
+        >
+            <NavbarItem>
+                <Link
+                    color="foreground"
+                    href="#"
+                >
+                    Features
+                </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+                <Link
+                    href="#"
+                    aria-current="page"
+                >
+                    Customers
+                </Link>
+            </NavbarItem>
+            <NavbarItem>
+                <Link
+                    color="foreground"
+                    href="#"
+                >
+                    Integrations
+                </Link>
+            </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+            <NavbarItem className="flex">
+                <Link href="#">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+                <Button
+                    as={Link}
+                    color="primary"
+                    href="#"
+                    variant="flat"
+                >
+                    Sign Up
+                </Button>
+            </NavbarItem>
+            <NavbarMenuToggle className="lg:hidden" />
+        </NavbarContent>
+        <NavbarMenu>
+            {menuItems.map((item, index) => (
+                <NavbarMenuItem key={`${item}-${index}`}>
+                    <Link
+                        color={
+                            index === 2
+                                ? "primary"
+                                : index === menuItems.length - 1
+                                ? "danger"
+                                : "foreground"
+                        }
+                        className="w-full"
+                        href="#"
+                        size="lg"
+                    >
+                        {item}
+                    </Link>
+                </NavbarMenuItem>
+            ))}
+        </NavbarMenu>
+    </Navbar>
+);
