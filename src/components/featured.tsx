bd0eaf0ac82364f3dafb8a701f775a255a1f61f2
cspencer <cspencernd@gmail.com>
2023-11-14 06:16:07 -0500

    feat: add dummy products images and names

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -9,12 +9,17 @@ import { ArrowRight } from "lucide-react";
 import { faker } from "@faker-js/faker";
 
 export async function Featured() {
-    const featuredItems = faker.helpers.arrayElements(
-        [faker.image.urlLoremFlickr({ category: "product", height: 256, width: 256 })],
-        7,
-    );
+    const getProduct = () => {
+        return {
+            name: faker.commerce.product(),
+            image: faker.image.urlPicsumPhotos({
+                height: 256,
+                width: 256,
+            }),
+        };
+    };
 
-    console.log(featuredItems)
+    const featuredItems = faker.helpers.multiple(getProduct, { count: 7 });
 
     return (
         <>
@@ -24,27 +29,19 @@ export async function Featured() {
             <div className="relative">
                 <ScrollArea>
                     <ul className="flex space-x-4 pb-4">
-                        {featuredItems.map((f, i) => (
+                        {featuredItems.map(({ image, name }, i) => (
                             <li key={i}>
                                 <Image
                                     as={NextImage}
-                                    src={f}
+                                    src={image}
                                     alt={`product-${i}`}
                                     width={256}
                                     height={256}
+                                    classNames={{ img: "object-cover object-center h-full" }}
                                 />
+                                <h3>{name}</h3>
                             </li>
                         ))}
-                        {/* {listenNowAlbums.map((album) => ( */}
-                        {/*     <AlbumArtwork */}
-                        {/*         key={album.name} */}
-                        {/*         album={album} */}
-                        {/*         className="w-[250px]" */}
-                        {/*         aspectRatio="portrait" */}
-                        {/*         width={250} */}
-                        {/*         height={330} */}
-                        {/*     /> */}
-                        {/* ))} */}
                     </ul>
                     <ScrollBar orientation="horizontal" />
                 </ScrollArea>
