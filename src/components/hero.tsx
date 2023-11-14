ef7b1cbb050bc9c02a53892cf4d57ee8266211b9
cspencer <cspencernd@gmail.com>
2023-11-14 05:09:32 -0500

    style: fixup the sizing

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,16 +2,22 @@ import NextImage from "next/image";
 import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/button";
 
+import { ArrowRight } from "lucide-react";
+
 import { faker } from "@faker-js/faker";
 
-const headline = faker.company.catchPhrase();
-const descriptor = faker.lorem.paragraph();
-const banner = faker.image.urlLoremFlickr({ category: "computer" });
-
 export async function Hero() {
+    const headline = faker.company.catchPhrase();
+    const descriptor = faker.lorem.paragraph();
+    const banner = faker.image.urlLoremFlickr({
+        category: "abstract",
+        width: 800,
+        height: 450,
+    });
+
     return (
-        <div className="flex flex-col items-center gap-8 lg:flex-row">
-            <div className="prose relative isolate flex flex-col items-center text-center dark:prose-invert">
+        <div className="relative flex flex-col items-center gap-8 lg:flex-row">
+            <div className="prose relative isolate flex flex-col items-center text-center dark:prose-invert lg:max-w-[50ch] lg:items-start lg:text-left">
                 <h1 className="m-0 bg-gradient-to-br from-default-900 to-default-300 bg-clip-text text-transparent">
                     {headline}
                 </h1>
@@ -20,11 +26,12 @@ export async function Hero() {
                     color="primary"
                     variant="shadow"
                 >
-                    Get Started
+                    <ArrowRight size={16} />
+                    <span>Get Started</span>
                 </Button>
                 <div
                     aria-hidden="true"
-                    className=" absolute -z-10 h-3/4 w-full bg-gradient-to-b from-secondary to-cyan-600/60 opacity-40 blur-3xl"
+                    className="pointer-events-none absolute -z-10 h-3/4 w-full transform-gpu bg-gradient-to-b from-secondary to-cyan-600/60 opacity-40 blur-3xl"
                 />
             </div>
 
@@ -32,9 +39,9 @@ export async function Hero() {
                 as={NextImage}
                 src={banner}
                 alt="banner"
-                width={576}
-                height={576}
-                // fill
+                width={800}
+                height={450}
+                classNames={{img: "object-cover object-center h-full" }}
             />
         </div>
     );
