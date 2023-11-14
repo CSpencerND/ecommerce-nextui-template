8e813014662c0789e6896daa61376f3566a386cd
cspencer <cspencernd@gmail.com>
2023-11-14 05:10:47 -0500

    chore: add placeholder.com

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "loremflickr.com"
+            },
+            {
+                protocol: "https",
+                hostname: "placeholder.com"
             }
         ]
     }
