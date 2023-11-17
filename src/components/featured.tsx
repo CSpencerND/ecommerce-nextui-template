532841dd01f9a4864f89143545b2c0e9f50674d0
cspencer <cspencernd@gmail.com>
2023-11-17 11:34:19 -0500

    feat: add a copy section

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
 import { Carousel, CarouselItem } from "@/components/carousel-embla";
+import { Button } from "@nextui-org/button";
 
 import { Image } from "@nextui-org/image";
 import NextImage from "next/image";
@@ -10,22 +12,24 @@ import { useHydrated } from "@/hooks/use-hydrated";
 
 export function Featured() {
     useFaker((s) => s.setFeaturedItems());
-    const featuredItems = useFaker((s) => s.featuredItems);
+    const { copy, items } = useFaker((s) => s.featuredItems);
 
     const hydrated = useHydrated();
     if (!hydrated) return null;
 
     return (
-        <>
-            <header className="prose dark:prose-invert">
-                <h1>Featured</h1>
-            </header>
+        <div className="flex flex-col lg:flex-row gap-x-12">
+            <div className="prose text-center dark:prose-invert lg:hidden">
+                <h1 className="pb-4">{copy.adjective}</h1>
+            </div>
+
             <Carousel
-                numSlides={featuredItems.length}
+                numSlides={items.length}
                 loop
                 showControls
+                className="lg:max-w-6xl"
             >
-                {featuredItems.map(({ image, name }, i) => (
+                {items.map(({ image, name }, i) => (
                     <CarouselItem
                         key={i}
                         index={i}
@@ -42,7 +46,21 @@ export function Featured() {
                     </CarouselItem>
                 ))}
             </Carousel>
-        </>
+
+            <div className="prose relative isolate flex flex-col items-center justify-center dark:prose-invert lg:items-start lg:text-left">
+                <h1 className="m-0 bg-gradient-to-br from-default-900 to-default-300 bg-clip-text text-transparent max-lg:hidden">
+                    {copy.adjective}
+                </h1>
+                <p className="">{copy.description}</p>
+                <Button
+                    color="primary"
+                    variant="shadow"
+                    className="lg:self-end"
+                >
+                    See More
+                </Button>
+            </div>
+        </div>
     );
 
     // return (
