3404cc974909f4a1d9b9bb041f9b1e277a62ad2a
cspencer <cspencernd@gmail.com>
2023-11-17 14:56:50 -0500

    chore: rm comments

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
 import { Carousel, CarouselItem } from "@/components/carousel-embla";
 import { Button } from "@nextui-org/button";
 
@@ -62,30 +61,4 @@ export function Featured() {
             </div>
         </div>
     );
-
-    // return (
-    //     <>
-    //         <div className="prose dark:prose-invert">
-    //             <h1>Featured</h1>
-    //         </div>
-    //         <div className="relative w-full min-h-[192px] max-w-prose rounded-2xl border border-content2 bg-content1 p-3">
-    //             <Carousel>
-    //                 {featuredItems.map(({ image, name }, i) => (
-    //                     <Image
-    //                         onDrag={(e) => e.preventDefault()}
-    //                         key={i}
-    //                         as={NextImage}
-    //                         src={image}
-    //                         alt={name}
-    //                         width={192}
-    //                         height={192}
-    //                         className="w-full h-full object-contain object-center !static"
-    //                         // classNames={{img: "w-full h-full object-contain object-center !static"}}
-    //                         radius="sm"
-    //                     />
-    //                 ))}
-    //             </Carousel>
-    //         </div>
-    //     </>
-    // );
 }
