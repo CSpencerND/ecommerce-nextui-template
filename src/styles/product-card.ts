d059f99583bb573b15317956947ab8f51c7c5703
cspencer <cspencernd@gmail.com>
2023-11-24 22:47:13 -0500

    style/feat: ad prouct-card styles

---
+++ src/styles/product-card.ts
@@ -0,0 +1,29 @@
+import { cn } from "@nextui-org/react";
+import { tv } from "tailwind-variants";
+
+export const cardFooter = tv({
+    base: cn(
+        "absolute z-10 ml-1 rounded-medium border-1 border-white/20 bg-white/10 py-1 shadow-small",
+        "[&>h3]:truncate [&>h3]:text-medium [&>h3]:font-bold [&>h3]:text-white",
+    ),
+    variants: {
+        hasPadding: {
+            true: "bottom-4 w-[calc(100%-2rem)]",
+            default: "bottom-1 w-[calc(100%-0.5rem)]",
+        },
+    },
+    defaultVariants: {
+        hasPadding: "default",
+    },
+});
+
+export const cardImage = tv({
+    base: "data-[loaded=true]:bg-stripe-gradient",
+});
+
+export const card = {
+    footer: cardFooter,
+    image: cardImage,
+};
+
+export default card;
