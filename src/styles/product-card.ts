4c9e0e2fbced483585626bf42e3dd72df07e9a56
cspencer <cspencernd@gmail.com>
2023-11-24 23:19:25 -0500

    perf: change to array cuz realized cn not neede

--- src/styles/product-card.ts
+++ src/styles/product-card.ts
@@ -1,11 +1,10 @@
-import { cn } from "@nextui-org/react";
 import { tv } from "tailwind-variants";
 
 export const cardFooter = tv({
-    base: cn(
+    base: [
         "absolute z-10 ml-1 rounded-medium border-1 border-white/20 bg-white/10 py-1 shadow-small",
         "[&>h3]:truncate [&>h3]:text-medium [&>h3]:font-bold [&>h3]:text-white",
-    ),
+    ],
     variants: {
         hasPadding: {
             true: "bottom-4 w-[calc(100%-2rem)]",
