e03d02f9bbab55bf9c86350f498910644d7280e8
cspencer <cspencernd@gmail.com>
2023-11-25 17:35:07 -0500

    fix/feat: add styles dir to tailwind config content array

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
