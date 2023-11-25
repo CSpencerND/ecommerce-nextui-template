98d6d87795b33d9f634ea41cbdd6f7369284f5b0
cspencer <cspencernd@gmail.com>
2023-11-24 23:36:02 -0500

    misc: experiment with slots

--- src/styles/product-card.ts
+++ src/styles/product-card.ts
@@ -25,4 +25,27 @@ export const card = {
     image: cardImage,
 };
 
+// const card = tv({
+//     slots: {
+//         footer: [
+//             "absolute z-10 ml-1 rounded-medium border-1 border-white/20 bg-white/10 py-1 shadow-small",
+//             "[&>h3]:truncate [&>h3]:text-medium [&>h3]:font-bold [&>h3]:text-white",
+//         ],
+//         img: "data-[loaded=true]:bg-stripe-gradient",
+//     },
+//     variants: {
+//         hasPadding: {
+//             true: {
+//                 footer: "bottom-4 w-[calc(100%-2rem)]",
+//             },
+//             default: {
+//                 footer: "bottom-1 w-[calc(100%-0.5rem)]",
+//             },
+//         },
+//     },
+//     defaultVariants: {
+//         hasPadding: "default",
+//     },
+// });
+
 export default card;
