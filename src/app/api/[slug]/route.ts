20d6e2e89de7f9492f4b25df076977dacea83ac1
cspencer <cspencernd@gmail.com>
2023-11-26 13:39:38 -0500

    refactor: rework the api route to be dynamic; allows additional get handler without the need to create a new route;

---
+++ src/app/api/[slug]/route.ts
@@ -0,0 +1,14 @@
+import { fakerFunctions, type ApiType } from "@/faker/faker-functions";
+import { NextResponse } from "next/server";
+
+type RouteParams = {
+    params: {
+        slug: keyof ApiType;
+    };
+};
+
+export async function GET(_req: Request, { params }: RouteParams) {
+    const getData = fakerFunctions[params.slug];
+    const data = getData();
+    return NextResponse.json(data);
+}
