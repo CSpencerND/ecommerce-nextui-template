c65d9830103329e0d60dc3e4a6d3c4c16b948fbf
cspencer <cspencernd@gmail.com>
2023-11-25 19:04:10 -0500

    style: copy the title variants from nextui template

--- src/components/primitives.ts
+++ src/components/primitives.ts
@@ -10,7 +10,27 @@ export const section = tv({
         },
     },
 });
- 
+
 export const title = tv({
-    base: "m-0 bg-gradient-to-br from-default-900 to-default-400 bg-clip-text text-transparent",
+    base: "m-0",
+    variants: {
+        color: {
+            violet: "from-[#FF1CF7] to-[#b249f8]",
+            yellow: "from-[#FF705B] to-[#FFB457]",
+            blue: "from-[#5EA2EF] to-[#0072F5]",
+            cyan: "from-[#00b7fa] to-[#01cfea]",
+            green: "from-[#6FEE8D] to-[#17c964]",
+            pink: "from-[#FF72E1] to-[#F54C7A]",
+            fg: "from-foreground to-foreground/60",
+        },
+    },
+    defaultVariants: {
+        color: "fg"
+    },
+    compoundVariants: [
+        {
+            color: ["violet", "yellow", "blue", "cyan", "green", "pink", "fg"],
+            class: "bg-gradient-to-b bg-clip-text text-transparent",
+        },
+    ],
 });
