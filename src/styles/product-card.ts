dc20c0b2e37460104d819844b52bccb27dbaa16a
cspencer <cspencernd@gmail.com>
2023-11-27 23:18:47 -0500

    style/perf: attempt to improve readability

--- src/styles/product-card.ts
+++ src/styles/product-card.ts
@@ -19,8 +19,9 @@ export const cardRoot = tv({
 
 export const cardTitle = tv({
     base: [
-        "absolute z-10 ml-1 rounded-[10px] border-1 border-white/20 bg-white/10 py-1 shadow-small",
+        "absolute z-10 ml-1 rounded-[10px] border-1 border-white/10 bg-white/10 py-1 shadow-small",
         "[&>h3]:truncate [&>h3]:text-medium [&>h3]:font-bold [&>h3]:text-white",
+        "mix-blend-difference",
     ],
     variants: {
         hasPadding: {
