2903fd97d3063dbc2b7569c19670e9da57062951
cspencer <cspencernd@gmail.com>
2023-11-27 23:39:17 -0500

    style/perf: contrast better than mix-blend for this situation

--- src/styles/product-card.ts
+++ src/styles/product-card.ts
@@ -19,9 +19,9 @@ export const cardRoot = tv({
 
 export const cardTitle = tv({
     base: [
-        "absolute z-10 ml-1 rounded-[10px] border-1 border-white/10 bg-white/10 py-1 shadow-small",
+        "absolute z-10 ml-1 rounded-[10px] border-1 border-white/10",
+        "bg-white/10 py-1 shadow-small backdrop-contrast-75",
         "[&>h3]:truncate [&>h3]:text-medium [&>h3]:font-bold [&>h3]:text-white",
-        "mix-blend-difference",
     ],
     variants: {
         hasPadding: {
