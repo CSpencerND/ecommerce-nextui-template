1c956169d3a797ac7660002bc98cad621555a8b8
cspencer <cspencernd@gmail.com>
2023-12-03 22:21:46 -0500

    feat/style: add layout and styling

--- src/app/collections/[collection-slug]/[product-slug]/page.tsx
+++ src/app/collections/[collection-slug]/[product-slug]/page.tsx
@@ -14,55 +14,56 @@ export default async function ProductPage() {
 
     return (
         <section className={section()}>
-            <Card className="rounded-xlarge">
-                <CardBody className="space-y-6">
-                    <div className="w-full">
-                        <Image
-                            as={NextImage}
-                            src={images[0]}
-                            alt={name}
-                            width={192}
-                            height={192}
-                            className={card.image()}
-                        />
-                    </div>
-                    <div className={prose()}>
-                        <h2 className="pb-6">{name}</h2>
-                        <p>{description}</p>
-                        <p>{price}</p>
-                    </div>
+            <Card className="max-w-4xl rounded-xlarge">
+                <CardBody className="gap-6 max-md:max-w-min md:flex-row md:items-center md:p-6">
+                    <Image
+                        as={NextImage}
+                        src={images[0]}
+                        alt={name}
+                        width={384}
+                        height={384}
+                        classNames={{ wrapper: "flex-none", img: card.image() }}
+                        sizes="100vw"
+                    />
+                    <div className="flex flex-col gap-6 basis-0">
+                        <div className={prose()}>
+                            <h2 className="pb-6">{name}</h2>
+                            <p>{description}</p>
+                            <p>{price}</p>
+                        </div>
 
-                    {/** TODO: Obviously will be ColorSwatch */}
-                    <div className="inline-flex gap-3">
-                        {colors.map((color, i) => (
-                            <Button
-                                key={i}
-                                size="sm"
-                                isIconOnly
-                                style={{ backgroundColor: color }}
-                            >
-                                <span className="sr-only">{color}</span>
-                            </Button>
-                        ))}
-                    </div>
+                        {/** TODO: Obviously will be ColorSwatch */}
+                        <div className="inline-flex gap-3">
+                            {colors.map((color, i) => (
+                                <Button
+                                    key={i}
+                                    size="sm"
+                                    isIconOnly
+                                    style={{ backgroundColor: color }}
+                                >
+                                    <span className="sr-only">{color}</span>
+                                </Button>
+                            ))}
+                        </div>
 
-                    {/** TODO: Change this to RadioGroup */}
-                    <div className="inline-flex gap-3">
-                        {sizes.map((size, i) => (
-                            <Button
-                                key={i}
-                                size="sm"
-                                variant="flat"
-                                isIconOnly
-                            >
-                                {size}
-                            </Button>
-                        ))}
-                    </div>
+                        {/** TODO: Change this to RadioGroup */}
+                        <div className="inline-flex gap-3">
+                            {sizes.map((size, i) => (
+                                <Button
+                                    key={i}
+                                    size="sm"
+                                    variant="flat"
+                                    isIconOnly
+                                >
+                                    {size.toLowerCase()}
+                                </Button>
+                            ))}
+                        </div>
 
-                    <div className="inline-flex gap-3">
-                        <Button>Buy Now</Button>
-                        <Button>Add To Bag</Button>
+                        <div className="inline-flex gap-3 max-w-min [&>*]:flex-1">
+                            <Button color="primary">Buy Now</Button>
+                            <Button color="primary" variant="bordered">Add To Bag</Button>
+                        </div>
                     </div>
                 </CardBody>
             </Card>
