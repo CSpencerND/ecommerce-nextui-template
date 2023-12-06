c40d2c8174df2fa42dc12e9926ab797af3581371
cspencer <cspencernd@gmail.com>
2023-12-06 03:22:41 -0500

    feat: add radix/shadcn toggle and toggle-group, and customize to work with nextui

---
+++ src/components/ui/toggle-group.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
+import * as React from "react";
+
+import { toggleVariants } from "@/components/ui/toggle";
+import { cn } from "@nextui-org/react";
+import { type VariantProps } from "@nextui-org/theme";
+
+const ToggleGroupContext = React.createContext<VariantProps<typeof toggleVariants>>({
+    size: "default",
+    variant: "default",
+    isSquared: false,
+});
+
+type ToggleGroupProps = React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
+    VariantProps<typeof toggleVariants>;
+
+const ToggleGroup = React.forwardRef<
+    React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+    ToggleGroupProps
+>(({ className, variant, size, isSquared, children, ...props }, ref) => (
+    <ToggleGroupPrimitive.Root
+        ref={ref}
+        className={cn("flex items-center gap-3", className)}
+        {...props}
+    >
+        <ToggleGroupContext.Provider value={{ variant, size, isSquared }}>
+            {children}
+        </ToggleGroupContext.Provider>
+    </ToggleGroupPrimitive.Root>
+));
+
+ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName;
+
+type ToggleGroupItemProps = React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
+    VariantProps<typeof toggleVariants>;
+
+const ToggleGroupItem = React.forwardRef<
+    React.ElementRef<typeof ToggleGroupPrimitive.Item>,
+    ToggleGroupItemProps
+>(({ className, children, variant, size, isSquared, ...props }, ref) => {
+    const context = React.useContext(ToggleGroupContext);
+
+    return (
+        <ToggleGroupPrimitive.Item
+            ref={ref}
+            className={toggleVariants({
+                variant: context.variant ?? variant,
+                size: context.size ?? size,
+                isSquared: context.isSquared ?? isSquared,
+                className,
+            })}
+            {...props}
+        >
+            {children}
+        </ToggleGroupPrimitive.Item>
+    );
+});
+
+ToggleGroupItem.displayName = ToggleGroupPrimitive.Item.displayName;
+
+export { ToggleGroup, ToggleGroupItem };
+export type { ToggleGroupItemProps, ToggleGroupProps };
