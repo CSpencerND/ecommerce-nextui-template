95435d7a7ad7646f7440c762a8583bc38c31b886
cspencer <cspencernd@gmail.com>
2023-12-06 03:21:56 -0500

    style/feat: add a size utility which sets width and height equally

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,43 @@ const config: Config = {
                 icon: "22.37%",
             },
         },
+        size: {
+            "0": "0px",
+            px: "1px",
+            "0.5": "0.125rem",
+            1: "0.25rem",
+            "1.5": "0.375rem",
+            2: "0.5rem",
+            "2.5": "0.625rem",
+            3: "0.75rem",
+            "3.5": "0.875rem",
+            4: "1rem",
+            5: "1.25rem",
+            6: "1.5rem",
+            7: "1.75rem",
+            8: "2rem",
+            9: "2.25rem",
+            10: "2.5rem",
+            11: "2.75rem",
+            12: "3rem",
+            14: "3.5rem",
+            16: "4rem",
+            20: "5rem",
+            24: "6rem",
+            28: "7rem",
+            32: "8rem",
+            36: "9rem",
+            40: "10rem",
+            44: "11rem",
+            48: "12rem",
+            52: "13rem",
+            56: "14rem",
+            60: "15rem",
+            64: "16rem",
+            72: "18rem",
+            80: "20rem",
+            96: "24rem",
+        },
     },
     darkMode: "class",
     plugins: [
@@ -48,7 +85,7 @@ const config: Config = {
                 dim: themeColorsDim,
             },
         }),
-        plugin(function ({ addUtilities, addVariant }) {
+        plugin(function ({ addUtilities, matchUtilities, addVariant, theme }) {
             addUtilities({
                 ".h-screen-d": {
                     height: ["100vh", "100dvh"],
@@ -63,6 +100,17 @@ const config: Config = {
                     outline: "1px solid red",
                 },
             });
+
+            matchUtilities(
+                {
+                    size: (value) => ({
+                        width: value,
+                        height: value,
+                    }),
+                },
+                { values: theme("size") },
+            );
+
             /** HACK: Experimental - May not work as expected */
             addVariant("focus-visible-within", "&:has(:focus-visible)");
         }),
