b9ff2c78ffe8c49700b03ae041def85c741e2750
cspencer <cspencernd@gmail.com>
2023-12-06 10:11:57 -0500

    style: directly add the bg stripe gradient

--- src/components/preview-image.tsx
+++ src/components/preview-image.tsx
@@ -1,9 +1,9 @@
 import { Image, type ImageProps } from "@nextui-org/react";
 import NextImage from "next/image";
 
-import { cardImage } from "@/styles/product-card";
+import { cn } from "@nextui-org/react";
 
-export function ProductImage({ src, alt, ...props }: ImageProps) {
+export function ProductImage({ src, alt, className, ...props }: ImageProps) {
     return (
         <Image
             as={NextImage}
@@ -12,7 +12,7 @@ export function ProductImage({ src, alt, ...props }: ImageProps) {
             width={192}
             height={192}
             sizes="144, (min-width: 376px) 192px"
-            className={cardImage()}
+            className={cn("data-[loaded=true]:bg-stripe-gradient", className)}
             isZoomed
             {...props}
         />
