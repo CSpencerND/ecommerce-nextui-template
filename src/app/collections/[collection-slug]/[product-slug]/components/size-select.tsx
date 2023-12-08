e126cd66c758b57b9e4f622364ee01a4ea98ca4c
cspencer <cspencernd@gmail.com>
2023-12-08 04:18:30 -0500

    feat(size-select): implement use-query-string

--- src/app/collections/[collection-slug]/[product-slug]/components/size-select.tsx
+++ src/app/collections/[collection-slug]/[product-slug]/components/size-select.tsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useQueryString } from "@/hooks/use-query-string";
 import { useState } from "react";
 
 type SizeSelectProps = {
@@ -11,13 +12,17 @@ type SizeSelectProps = {
 export function SizeSelect({ sizes, isSquared, ...props }: SizeSelectProps) {
     const [value, setValue] = useState(sizes[0]);
 
+    const { updatePathname } = useQueryString("size", value);
+
     return (
         <ToggleGroup
             type="single"
             size="sm"
             value={value}
             onValueChange={(value) => {
-                if (value) setValue(value);
+                if (!value) return;
+                setValue(value);
+                updatePathname();
             }}
             isSquared={isSquared}
             className="max-sm:flex-wrap"
