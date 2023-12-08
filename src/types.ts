f9554e515e103cac131956ba09aba98974551824
cspencer <cspencernd@gmail.com>
2023-12-08 04:18:46 -0500

    feat(types): add Colors type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,8 @@
+import type { ApiType } from "@/faker/faker-functions";
 import type { SVGProps } from "react";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
     size?: number;
 };
-
-export type { SiteConfig } from "@/site.config"
+export type { SiteConfig } from "@/site.config";
+export type Colors = ApiType["product"]["colors"];
