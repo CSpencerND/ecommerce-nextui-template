0c72dafcabfcf7712f4fae7ff51b07acca6083f9
cspencer <cspencernd@gmail.com>
2023-12-19 20:13:20 -0500

    update tailwind and remove homemade size classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import { nextui } from "@nextui-org/react";
-import typography from "@tailwindcss/typography";
 import container from "@tailwindcss/container-queries";
+import typography from "@tailwindcss/typography";
 import plugin from "tailwindcss/plugin";
 
 import { themeColorsDark, themeColorsDim, themeColorsLight } from "./src/theme";
@@ -31,44 +31,6 @@ const config: Config = {
                 icon: "22.37%",
             },
         },
-        /** NOTE: @update will not be needed on the next update */
-        size: {
-            "0": "0px",
-            px: "1px",
-            "0.5": "0.125rem",
-            1: "0.25rem",
-            "1.5": "0.375rem",
-            2: "0.5rem",
-            "2.5": "0.625rem",
-            3: "0.75rem",
-            "3.5": "0.875rem",
-            4: "1rem",
-            5: "1.25rem",
-            6: "1.5rem",
-            7: "1.75rem",
-            8: "2rem",
-            9: "2.25rem",
-            10: "2.5rem",
-            11: "2.75rem",
-            12: "3rem",
-            14: "3.5rem",
-            16: "4rem",
-            20: "5rem",
-            24: "6rem",
-            28: "7rem",
-            32: "8rem",
-            36: "9rem",
-            40: "10rem",
-            44: "11rem",
-            48: "12rem",
-            52: "13rem",
-            56: "14rem",
-            60: "15rem",
-            64: "16rem",
-            72: "18rem",
-            80: "20rem",
-            96: "24rem",
-        },
     },
     darkMode: "class",
     plugins: [
@@ -93,7 +55,7 @@ const config: Config = {
                 dim: themeColorsDim,
             },
         }),
-        plugin(function ({ addUtilities, matchUtilities, addVariant, theme }) {
+        plugin(function ({ addUtilities, addVariant }) {
             addUtilities({
                 /** NOTE: @update will not be needed on the next update */
                 ".h-screen-d": {
@@ -116,17 +78,6 @@ const config: Config = {
                     outlineColor: "hsl(var(--nextui-focus) / var(--nextui-focus-opacity, 1))",
                 },
             });
-
-            matchUtilities(
-                /** NOTE: @update will not be needed on the next update */
-                {
-                    size: (value) => ({
-                        width: value,
-                        height: value,
-                    }),
-                },
-                { values: theme("size") },
-            );
 
             /** HACK: Experimental - May not work as expected */
             addVariant("focus-visible-within", "&:has(:focus-visible)");
