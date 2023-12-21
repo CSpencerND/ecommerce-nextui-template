83edf0bf4d7110388824a10c1299a1572b607deb
cspencer <cspencernd@gmail.com>
2023-12-21 09:32:05 -0500

    feat(types): add Maybe and Product types

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,10 @@ export type IconSvgProps = SVGProps<SVGSVGElement> & {
     size?: number;
 };
 export type { SiteConfig } from "@/site.config";
+
+export type Maybe<T> = null | undefined | T;
+export type SearchParams<Key extends string> = Record<Key, string | string[] | undefined>;
+
+export type Product = ApiType["product"];
 export type Colors = ApiType["product"]["colors"];
 export type Sizes = ApiType["product"]["sizes"];
-export type SearchParams<Key extends string> = Record<Key, string | string[] | undefined>;
