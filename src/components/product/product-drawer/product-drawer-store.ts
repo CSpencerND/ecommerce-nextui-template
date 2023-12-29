68117952ee4457c827a956fb36f230162302d1f5
cspencer <cspencernd@gmail.com>
2023-12-29 04:45:45 -0500

    add setData function

--- src/components/product/product-drawer/product-drawer-store.ts
+++ src/components/product/product-drawer/product-drawer-store.ts
@@ -4,6 +4,7 @@ import type { Maybe, Product } from "@/types";
 
 type ProductDrawerStore = {
     data: Maybe<Product>;
+    setData: (data: Product) => void;
     isOpen: boolean;
     setIsOpen: (isOpen: boolean) => void;
     onOpen: (data: Product) => void;
@@ -12,6 +13,7 @@ type ProductDrawerStore = {
 
 export const useProductDrawer = create<ProductDrawerStore>()((set) => ({
     data: null,
+    setData: (data) => set({ data }),
     isOpen: false,
     setIsOpen: (isOpen) => set({ isOpen: isOpen }),
     onOpen: (data) => set({ data: data, isOpen: true }),
