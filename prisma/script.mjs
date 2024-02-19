fff0ba4210000774769497b117c8cc5a92d8ce94
cspencer <cspencernd@gmail.com>
2024-02-19 01:44:32 -0500

    feat?: add possibly useless prisma script

--- prisma/script.mjs
+++ prisma/script.mjs
@@ -4,40 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    const resImage = async () =>
-        await prisma.image.create({
-            data: {
-                src: "https://picsum.photos/seed/ao6APQ/1024/576",
-                alt: "banner",
-                height: 1024,
-                width: 576,
-            },
-        });
-
-    const resHero = async () =>
-        await prisma.hero.create({
-            data: {
-                headline: "Compatible regional info-mediaries",
-                description:
-                    "Arbitro adaugeo quia cito cometes laudantium. Nam iure ambitus tepesco ad uterque. Despecto cena compello suppono.",
-                image: {
-                    create: {
-                        src: "https://picsum.photos/seed/ao6APQ/1024/576",
-                        alt: "banner",
-                        height: 1024,
-                        width: 576,
-                    },
-                },
-            },
-        });
-
-    const resFind = async () =>
-        await prisma.hero.findMany({
-            include: {
-                image: true,
-            },
-        });
-
     // await clean();
     // console.log(await resFind());
 }
@@ -53,4 +19,33 @@ main()
 async function clean() {
     await prisma.hero.deleteMany();
     await prisma.image.deleteMany();
+}
+
+async function createImage() {
+    await prisma.image.create({
+        data: {
+            src: "https://picsum.photos/seed/ao6APQ/1024/576",
+            alt: "banner",
+            height: 1024,
+            width: 576,
+        },
+    });
+}
+
+async function createHero() {
+    await prisma.hero.create({
+        data: {
+            headline: "Compatible regional info-mediaries",
+            description:
+                "Arbitro adaugeo quia cito cometes laudantium. Nam iure ambitus tepesco ad uterque. Despecto cena compello suppono.",
+            image: {
+                create: {
+                    src: "https://picsum.photos/seed/ao6APQ/1024/576",
+                    alt: "banner",
+                    height: 1024,
+                    width: 576,
+                },
+            },
+        },
+    });
 }
