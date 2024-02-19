c27b4de21d286de0e81fc7a9c4140bb4dc215c3c
cspencer <cspencernd@gmail.com>
2024-02-19 02:09:16 -0500

    style: adjust collection preview card styles

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -20,22 +20,21 @@ export default async function CollectionDirectoryPage() {
                     <MotionListItem
                         key={`Collection ${i}`}
                         index={i}
-                        className="relative rounded-f4 border border-divider/10"
+                        className="relative aspect-square rounded-f4 border border-divider"
                     >
                         <Image
                             as={NextImage}
                             src={image.src}
-                            alt="Collection Preview Image"
+                            alt={image.alt}
                             fill
                             sizes="192px"
                             classNames={{
-                                wrapper: "!max-w-none",
-                                img: "!static",
+                                wrapper: "aspect-square size-full rounded-f4",
                             }}
                         />
                         <Link
                             href={`/${name.toLowerCase()}`}
-                            className="absolute inset-0 z-10 grid items-end rounded-f4 p-f2 focus-visible:focus-ring"
+                            className="absolute inset-0 z-10 grid items-end rounded-[calc(var(--fibo-4)_+_var(--ring-gap))] p-f2 focus-visible:focus-ring"
                         >
                             <div className="h-fit rounded-f3 border border-divider bg-black/20 p-f2 backdrop-blur backdrop-saturate-150">
                                 <h3 className="px-f1 font-bold">{name}</h3>
